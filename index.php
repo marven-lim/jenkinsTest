2fb0d1ba7a77a734d4314b78cfb02c57755eafc2
marven-lim <74282240+marven-lim@users.noreply.github.com>
2022-11-15 15:24:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
      	<label>User Name</label>
      	<input type="password" name="password" placeholder="Password"><br>
 
-     	<button type="submit">Login</button>
+     	<button type="submit" name="submit">Login</button>
      </form>
 </body>
 </html>

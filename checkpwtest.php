e284aacc47a7fa5f031b57ed0cff970d56c42faa
marven-lim <74282240+marven-lim@users.noreply.github.com>
2022-11-15 13:26:31 +0800

    Update checkpwtest.php

--- checkpwtest.php
+++ checkpwtest.php
@@ -4,9 +4,13 @@ include 'checkpw.php';
 
 final class checkpwtest extends TestCase
 {
-	public function test_credentials(): void
+	public function test_valid_credentials(): void
 	{
 		$this->assertTrue(isValidCreds("araradacius","letmein"));
+	}
+	public function test_invalid_credentials(): void
+	{
+		$this->assertFalse(isValidCreds("wow","letmein"));
 	}
 }
 

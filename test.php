838405f35de4f5bb64db4b56c8d96db06eb35d5c
marven-lim <74282240+marven-lim@users.noreply.github.com>
2022-11-15 11:03:49 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require 'login.php';
+
+class LoginTest extends TestCase {
+	public function testLogin(): void
+	{
+		$this->assertTrue(isValidCreds("araradacius", "letmein"))
+	}
+}
